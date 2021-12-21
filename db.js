1c80fdd6bce0be81fe14c3b1c3bf39d10a32b078
April Bates <a.madeline.bates@gmail.com>
2021-12-21 11:47:08 +0000

    New repo for project back end

---
+++ db.js
@@ -0,0 +1,21 @@
+//This file is used to establish our database
+require('dotenv/config') // require the dotenv/config at beginning of file
+const mongoose = require('mongoose');
+
+const {
+  DATABASE_URL
+} = process.env;
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+};
+
+const url = `${DATABASE_URL}`;
+
+mongoose.connect(url, options).then(function () {
+  console.log('MongoDB is connected');
+})
+  .catch(function (err) {
+    console.log(err);
+});
